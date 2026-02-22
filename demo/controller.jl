e67e55adacf5d0b99705371463e21de277a3782d
Avinash Kumar Deepak <avinash8655279@gmail.com>
2026-02-22 14:18:36 +0530

    Add controller demo

---
+++ demo/controller.jl
@@ -0,0 +1,49 @@
+#=
+controller.jl -- Bang-bang controller node for concore demo
+
+Julia equivalent of the Python demo/controller.py.
+
+Run from the node working directory (e.g., CZ/):
+    julia --project=/path/to/concore-jl /path/to/concore-jl/demo/controller.jl
+=#
+
+# Make the Concore package findable
+push!(LOAD_PATH, joinpath(@__DIR__, ".."))
+using Concore
+
+# Setpoint
+const ysp = 3.0
+
+"""
+    controller(ym) -> Vector{Float64}
+
+Bang-bang controller: if measurement is below setpoint, increase by 1%;
+otherwise decrease by 10%.
+"""
+function controller(ym::Vector{Float64})::Vector{Float64}
+    if ym[1] < ysp
+        return 1.01 .* ym
+    else
+        return 0.9 .* ym
+    end
+end
+
+Concore.default_maxtime!(150)
+Concore.delay = 0.02
+
+init_simtime_u  = "[0.0, 0.0]"
+init_simtime_ym = "[0.0, 0.0]"
+
+u  = initval(init_simtime_u)
+ym = initval(init_simtime_ym)
+
+while Concore.simtime < Concore.maxtime
+    while unchanged()
+        ym = concore_read(1, "ym", init_simtime_ym)
+    end
+    u = controller(ym)
+    println("$(Concore.simtime). u=$(u) ym=$(ym)")
+    concore_write(1, "u", u; delta=0)
+end
+
+println("retry=$(Concore.retrycount)")
